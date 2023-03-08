56baa666f3bc9ab40ef3033b9e459a5231a014b3
PavanKumar <66913595+ascender1729@users.noreply.github.com>
2023-03-08 15:43:15 +0530

    Update nftmarketplace.sol

--- contracts/nftmarketplace.sol
+++ contracts/nftmarketplace.sol
@@ -1,47 +1,53 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract NFTMarketplace is ERC721 {
+contract NFTMarketplace is ERC721, ERC721Holder {
     using SafeMath for uint256;
 
-    struct NFT {
-        string metadata;
-        uint256 price;
-        address owner;
-        bool forSale;
+    struct NFTMetadata {
+        string name;
+        string description;
+        string image;
+        address creator;
     }
 
-    mapping(uint256 => NFT) public nfts;
+    mapping(uint256 => address) private nftOwners;
+    mapping(uint256 => uint256) private nftPrices;
+    mapping(uint256 => NFTMetadata) private nftMetadata;
 
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+    constructor() ERC721("NFT Marketplace", "NFTM") {}
 
-    function createNFT(uint256 _id, string memory _metadata, uint256 _price) public {
-        require(msg.sender == owner(), "Only contract owner can create NFTs");
-        require(nfts[_id].owner == address(0), "NFT with this ID already exists");
-        
-        nfts[_id] = NFT(_metadata, _price, address(0), false);
-        _mint(msg.sender, _id);
+    function createNFT(string memory name, string memory description, string memory image, uint256 price) public {
+        uint256 tokenId = totalSupply();
+        _safeMint(msg.sender, tokenId);
+        nftOwners[tokenId] = msg.sender;
+        nftPrices[tokenId] = price;
+        nftMetadata[tokenId] = NFTMetadata(name, description, image, msg.sender);
     }
 
-    function listForSale(uint256 _id, uint256 _price) public {
-        require(nfts[_id].owner == msg.sender, "Only the owner can list the NFT for sale");
-        require(!nfts[_id].forSale, "NFT is already listed for sale");
+    function buyNFT(uint256 tokenId) public payable {
+        require(_exists(tokenId), "NFT doesn't exist");
+        require(nftOwners[tokenId] != msg.sender, "You can't buy your own NFT");
+        require(msg.value >= nftPrices[tokenId], "Insufficient funds");
+        address seller = nftOwners[tokenId];
+        nftOwners[tokenId] = msg.sender;
+        nftPrices[tokenId] = nftPrices[tokenId];
+    _transfer(seller, msg.sender, tokenId);
+    payable(seller).transfer(msg.value);
+}
 
-        nfts[_id].forSale = true;
-        nfts[_id].price = _price;
-    }
+function getNFTMetadata(uint256 tokenId) public view returns (string memory, string memory, string memory, address) {
+    require(_exists(tokenId), "NFT doesn't exist");
+    NFTMetadata memory metadata = nftMetadata[tokenId];
+    return (metadata.name, metadata.description, metadata.image, metadata.creator);
+}
 
-    function purchaseNFT(uint256 _id) public payable {
-        require(nfts[_id].forSale, "NFT is not for sale");
-        require(msg.value >= nfts[_id].price, "Insufficient funds to purchase NFT");
-
-        address payable seller = payable(nfts[_id].owner);
-        seller.transfer(msg.value);
-
-        _transfer(seller, msg.sender, _id);
-        nfts[_id].owner = msg.sender;
-        nfts[_id].forSale = false;
-    }
+function getNFTPrice(uint256 tokenId) public view returns (uint256) {
+    require(_exists(tokenId), "NFT doesn't exist");
+    return nftPrices[tokenId];
+}
 }
